13e264c0404740d1566eae9dd14a49bdeb88522e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-05-18 06:51:22 +0530

    Update and rename lineage_X01BD.mk to infinity_X01BD.mk

--- infinity_X01BD.mk
+++ infinity_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The infinityOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,17 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinityOS stuff
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-MATRIXX_MAINTAINER := ManitnjG
+INFINITY_MAINTAINER := ManitnjG
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := infinity_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
